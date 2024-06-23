fba3e4a5d019d9bf02460a6ed7b1f79485394441
beingyash <beingyash2712@gmail.com>
2024-06-23 13:04:51 +0530

    started creating models

---
+++ CabApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabApplication.class, args);
+	}
+
+}
---
+++ model/Admin.java
@@ -0,0 +1,23 @@
+package com.spring.cab.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin extends User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer adminId ;	
+	
+}
